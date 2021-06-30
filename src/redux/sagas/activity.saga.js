61ab394c16af60228e550c4f57d25907124c5609
pthomas0309 <preston.thomas355@gmail.com>
2021-06-30 00:49:36 -0500

    construct dsaga to handle delete

--- src/redux/sagas/activity.saga.js
+++ src/redux/sagas/activity.saga.js
@@ -27,8 +27,8 @@ function* fetchActivity() {
 
         console.log('Error in activity GET', err);
 
-    }
-}
+    };
+};
 
 // worker Saga: will be fired on "ADD_NEW_ACTIVITY" actions
 function* addActivity(action) {
@@ -56,8 +56,37 @@ function* addActivity(action) {
 
         console.log('Error in activity POST', err);
 
-    }
-}
+    };
+};
+
+// worker Saga: will be fired on "DELETE_ACTIVITY" actions
+function* deleteActivity(action) {
+
+    // try executes until error
+    try {
+
+        // the config includes credentials which
+        // allow the server session to recognize the user
+        const config = {
+        headers: { 'Content-Type': 'application/json' },
+        withCredentials: true,
+        };
+
+        // axios POST for adding an activity to the user_activity table
+        yield axios.delete('/api/activity', action.payload, config);
+
+        // run the saga to GET all activity
+        yield put({ type: 'FETCH_ACTIVITY' });
+
+    } 
+
+    // catch executes after error in try
+    catch (err) {
+
+        console.log('Error in activity DELETE', err);
+
+    };
+};
 
 // listener for saga actions
 function* activitySaga() {
@@ -67,6 +96,9 @@ function* activitySaga() {
 
     // listen for 'ADD_NEW_ACTIVITY' action then run addActivity
     yield takeLatest('ADD_NEW_ACTIVITY', addActivity);
+
+    // listen for 'FETCH_ACTIVITY' action then run fetchActivity
+    yield takeLatest('DELETE_ACTIVITY', deleteActivity);
 
 }
 
