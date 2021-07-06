1ae05d15e847471343924b4d801bae6edfcab66a
pthomas0309 <preston.thomas355@gmail.com>
2021-07-06 09:30:46 -0500

    bugfix: source reducer in root reducer

--- src/redux/reducers/duration.reducer.js
+++ src/redux/reducers/duration.reducer.js
@@ -1,16 +1,19 @@
-const durationReducer = (state = { queue: [], isUpdating: {}, }, action) => {
+const durationReducer = (state = { queue: [], isUpdating: {} }, action) => {
 
     // switch to handle different action.type
     switch (action.type) {
         case 'UPDATE_QUEUE':
-            return {queue: [...state,queue, action.payload], ...state};
+            return {...state, queue: [...state.queue, action.payload]};
         case 'CLEAR_QUEUE':
-            return {queue: [], ...state};
+            return {...state, queue: []};
         case 'UPDATE_DURATION':
-            return {isUpdating: {...state,isUpdating, [action.payload.id]: true}, ...state};
+            console.log(action.payload.id);
+            return {...state, isUpdating: {...state.isUpdating, [action.payload.id]: true}};
         case 'UPDATE_FINISHED':
-            return {isUpdating: {...state,isUpdating, [action.payload.id]: false}, ...state}
+            return {...state, isUpdating: {...state.isUpdating, [action.payload.id]: false}}
         default:
             return state;
     };
 };
+
+export default durationReducer;
